d4870f3362a907bc33de18514fc4aefad8b8b863
heum23 <jinsoon6230@naver.com>
2025-05-09 14:57:55 +0900

    feat sns 디테일 페이지 데이터 요청

--- src/features/SnsMain/SnsDetail/index.tsx
+++ src/features/SnsMain/SnsDetail/index.tsx
@@ -1,8 +1,20 @@
+import { useEffect } from "react";
 import { SnsDetailStyled } from "./styled";
 import { useRouter } from "next/router";
+import api from "@/util/api";
 const SnsDetail = () => {
   const router = useRouter();
   const { id } = router.query;
+  //sns 디테일 페이지 이동 시 데이터 요청 , 게시글 모든 데이터 필요(잘 정리해서 줄것: 맘에 안들면 다시 )
+  useEffect(() => {
+    api
+      .get("/album/detailData", {
+        params: { SnsId: id },
+      })
+      .then((res) => {
+        console.log(res.data);
+      });
+  }, []);
   return (
     <SnsDetailStyled>
       <div>{id}</div>
