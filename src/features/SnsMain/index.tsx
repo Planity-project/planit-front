c70707091c4daeb535fd0aa9b2aa916985dca522
heum23 <jinsoon6230@naver.com>
2025-05-18 18:51:34 +0900

    feat: snsmain post요청

--- src/features/SnsMain/index.tsx
+++ src/features/SnsMain/index.tsx
@@ -3,47 +3,52 @@ import Busan from "@/assets/images/busan.jpeg";
 import expample from "@/assets/images/travel.jpg";
 import Image from "next/image";
 import SnsPost from "@/components/SnsPost";
+import { useEffect, useState } from "react";
+import api from "@/util/api";
 const SnsMain = () => {
+  const [data, setData] = useState<any[]>([]);
+  useEffect(() => {
+    api.get("/posts/list").then((res: any) => {
+      console.log(res.data); // 여기서 dummy 형식에 맞게 매핑 가능
+      setData(res.data);
+    });
+  }, []);
   const dummy = [
     {
       id: 1,
       userid: 1,
       nickName: "순흠",
       title: "여행",
-      hashtag: ["#부산", "#광안리"],
+      hashtag: "#부산",
       img: [Busan, Busan],
-      date: "2박 3일",
     },
     {
       id: 2,
       userid: 2,
       nickName: "순흠2",
       title: "여행1",
-      hashtag: ["#부산", "#광안리"],
+      hashtag: "#부산",
       img: [expample, expample, expample],
-      date: "2박 3일",
     },
     {
       id: 3,
       userid: 3,
       nickName: "순흠3",
       title: "여행2",
-      hashtag: ["#부산", "#광안리"],
+      hashtag: "#부산",
       img: [Busan, expample, expample],
-      date: "2박 3일",
     },
     {
       id: 4,
       userid: 4,
       nickName: "순흠4",
       title: "여행3",
-      hashtag: ["#부산", "#광안리"],
+      hashtag: "#부산",
       img: [expample, expample, expample, expample, expample],
-      date: "2박 3일",
     },
   ];
 
-  return <SnsPost data={dummy} />;
+  return <SnsPost data={data} />;
 };
 
 export default SnsMain;
