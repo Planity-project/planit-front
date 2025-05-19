050ba60b4a6db8d6a1770843d2a9ab38796f859a
hanbyeolson <nanbyeol7474@gmail.com>
2025-05-19 18:42:57 +0900

    Fix : 내정보 프로필 이미지 수정

--- src/features/MyPage/Myinfo/index.tsx
+++ src/features/MyPage/Myinfo/index.tsx
@@ -11,7 +11,9 @@ interface infoprops {
 const Myinfo = ({ user }: infoprops) => {
   const [name, setName] = useState<string>("");
   const [preview, setPreview] = useState<string>(
-    user?.profile_img ?? "/defaultImage.png"
+    user?.profile_img
+      ? `http://localhost:5001/${user?.profile_img}`
+      : "/defaultImage.png"
   );
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const fileInputRef = useRef<HTMLInputElement | null>(null);
