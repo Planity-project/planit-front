4008c11be8b5bcb6e4af2d7d659368b1a5745f25
ash <tkdgys1234@gmail.com>
2025-05-27 21:27:07 +0900

    Fix: url 수정

--- src/util/api.ts
+++ src/util/api.ts
@@ -1,13 +1,13 @@
 import { Modal } from "antd";
 import axios from "axios";
 
-const api = axios.create<any>({
-  baseURL: "http://localhost:5001", // ✅ 기본 주소 설정
+const api = axios.create({
+  baseURL: "http://planit.ai.kr/api", // ✅ 기본 주소 설정
   withCredentials: true, // 필요한 경우 쿠키 포함 요청
 });
 
-export const serverUrl = "http://locahost:5001";
-export const clientUrl = "http://localhost:3000";
+export const serverUrl = "https://planit.ai.kr/api";
+export const clientUrl = "https://planit.ai.kr";
 // api.interceptors.response.use(
 //   (response) => response,
 //   (error) => {
