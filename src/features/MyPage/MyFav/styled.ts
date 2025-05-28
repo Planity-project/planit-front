f20fc46f4054b1267c128e3551be26947e671899
heum23 <jinsoon6230@naver.com>
2025-05-29 00:11:31 +0900

    feat: 마이페이지 css

--- src/features/MyPage/MyFav/styled.ts
+++ src/features/MyPage/MyFav/styled.ts
@@ -1,13 +1,15 @@
 import styled from "styled-components";
 
 export const MyfavStyled = styled.div`
+  padding: 2rem;
   width: 100%;
   height: 100%;
   display: flex;
   justify-content: center;
 
   .myfav-wrap {
-    width: 70%;
+    max-width: 600px;
+    width: 100%;
     height: 100%;
     position: relative;
 
