540179acade61964ddfe72d7b1785e2fb3aec4ad
heum23 <jinsoon6230@naver.com>
2025-05-28 14:39:19 +0900

    feat: mypage

--- src/features/MyPage/index.tsx
+++ src/features/MyPage/index.tsx
@@ -1,64 +1,46 @@
-import { MypageStyled } from "./styled";
-import { useUser } from "@/context/UserContext";
-import Myinfo from "./Myinfo";
-import MyFav from "./MyFav";
-import { useEffect, useState } from "react";
-import Myinfodays from "./MyDays";
+// pages/mypage/[tab].tsx
 import { useRouter } from "next/router";
+import { useUser } from "@/context/UserContext";
+import Myinfo from "@/features/MyPage/Myinfo";
+import Myinfodays from "@/features/MyPage/MyDays";
+import MyFav from "@/features/MyPage/MyFav";
+import { MypageStyled } from "@/features/MyPage/styled";
+
 const MyPage = () => {
-  const router = useRouter();
+  const { tab } = useRouter().query;
   const { user } = useUser();
-  const [click, setClick] = useState(0);
-  useEffect(() => {
-    if (!router.isReady) return;
-    const { id } = router.query;
-    if (id === "1") setClick(0);
-    else if (id === "2") setClick(1);
-    else setClick(2);
-  }, [router.isReady, router.query]);
-  const handleTabClick = (index: number) => {
-    setClick(index);
-    // URL에서 쿼리 제거
-    router.replace("/mypage", undefined, { shallow: true });
+
+  const renderComponent = () => {
+    if (tab === "info") return <Myinfo user={user} />;
+    if (tab === "days") return <Myinfodays user={user} />;
+    if (tab === "fav") return <MyFav user={user} />;
+    return <div>잘못된 경로입니다.</div>;
   };
+
   return (
     <MypageStyled>
       <div className="mypage-wrap">
         <div className="mypage-sideBar">
           <div
-            onClick={() => {
-              handleTabClick(0);
-            }}
-            className={`mypage-myinfo ${click === 0 ? "active" : ""}`}
+            className={`mypage-myinfo ${tab === "info" ? "active" : ""}`}
+            onClick={() => location.replace("/mypage/info")}
           >
             내 정보
           </div>
           <div
-            onClick={() => {
-              handleTabClick(1);
-            }}
-            className={`mypage-mydays ${click === 1 ? "active" : ""}`}
+            className={`mypage-mydays ${tab === "days" ? "active" : ""}`}
+            onClick={() => location.replace("/mypage/days")}
           >
             내 일정
           </div>
           <div
-            onClick={() => {
-              handleTabClick(2);
-            }}
-            className={`mypage-likepost ${click === 2 ? "active" : ""}`}
+            className={`mypage-likepost ${tab === "fav" ? "active" : ""}`}
+            onClick={() => location.replace("/mypage/fav")}
           >
             관심 일정
           </div>
         </div>
-        <div className="mypage-component">
-          {click === 0 ? (
-            <Myinfo user={user} />
-          ) : click === 1 ? (
-            <Myinfodays user={user} />
-          ) : (
-            <MyFav user={user} />
-          )}
-        </div>
+        <div className="mypage-component">{renderComponent()}</div>
       </div>
     </MypageStyled>
   );
