d5512387a8cc98be31c7b7516c8fc45f7d0acd94
ash <tkdgys1234@gmail.com>
2025-05-29 22:49:07 +0900

    Fix:useContext 수정

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -52,7 +52,7 @@ export const UserProvider = ({ children }: UserProviderProps) => {
           reason?: string;
           endDate?: string;
         };
-
+        console.log(data, "data");
         if (data.result && data.user) {
           setUser(data.user);
 
